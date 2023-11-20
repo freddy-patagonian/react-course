afcf842c4b980ac92534c3d2ffa562d3a6c8d5ff
Freddy Orozco <forozco@bfaindustries.com>
2023-11-20 08:37:41 -0500

    lesson-2: Add base tic-tac-toe

--- lesson-2-essentials/tic-tac-toe/src/App.tsx
+++ lesson-2-essentials/tic-tac-toe/src/App.tsx
@@ -1,10 +1,24 @@
+import './index.css';
+
 import GlobalStyle from './global-style';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <p>Holi</p>
+      <main id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        Game Board
+      </main>
     </>
   );
 }
